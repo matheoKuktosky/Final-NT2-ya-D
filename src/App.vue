5e1bd97d1c1be8285b88e78872c48b6d08f02fef
matheoKuktosky <matheomike@outlook.com>
2021-07-13 20:15:10 -0300

    finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Codificacion/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Codificacion from './components/Codificacion.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Codificacion
   }
 }
 </script>
@@ -24,5 +23,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  float: left;
 }
 </style>
---
+++ src/components/Codificacion.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+    <h1>Matheo Kuktosky</h1>
+    <input 
+        type="text"
+        v-model="text" 
+    >
+    <h3>cantidad: {{text.length}}</h3>
+    <h3>{{ text | codificado }} (codificado)</h3>
+    <h3>{{ text.toUpperCase() }} (Mayúscula)</h3>
+    <h3>{{ text.toLowerCase() }} (Minúscula)</h3>
+    <h3>{{ text | mayusMin }} (Mayúscula/Minúscula)</h3>
+    <h3>{{ text | minMayus }} (Minúscula/Mayúscula)</h3>
+
+    <hr>
+
+    <p>respuestas: 1:b, 2:b, 3:c, 4:b, 5:b</p>
+    
+  </div>
+</template>
+
+
+<script>
+    export default{
+        data() {
+            return{
+                text: ''
+            }
+        }, 
+        filters: {
+            codificado: function (value) {
+                let newValue = value.toLowerCase()
+                let chars = {'a':'u','e':'o', 'o': 'e', 'u':'a'}
+                newValue = newValue.replace(/[aeou]/g, m => chars[m] ?? m)
+                return newValue
+            },
+            mayusMin: function (value) {
+                let chars = value.toLowerCase().split("");
+                for (let i = 0; i < chars.length; i += 2) {
+                    chars[i] = chars[i].toUpperCase();
+                }
+                return chars.join("");
+            },
+            minMayus: function (value) {
+                let chars = value.toUpperCase().split("");
+                for (let i = 0; i < chars.length; i += 2) {
+                    chars[i] = chars[i].toLowerCase();
+                }
+                return chars.join("");
+            }
+        }   
+    }   
+</script>
